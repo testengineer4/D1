59319369a82c296898fe73a569935fe1dc64d85f
unknown <seniortestengineer4@gmail.com>
2021-12-24 21:00:49 +0530

    i made changes in App.java file

--- java/com/new/application/RND/App.java
+++ java/com/new/application/RND/App.java
@@ -1,6 +1,10 @@
 package com.new.application.RND;
 
 public class App {
+	
+	int k;
+	String s1;
+	
 
     public static void main(String[] args) throws Exception {
         System.out.println("Hello World!");
